f8fa52bc1d377a3204dc33eab16503ed4ec77b7e
amaljithtr <amaljithtr@gmail.com>
2021-08-16 21:32:53 +0530

    Fixed parallel process issue

--- cucumber.js
+++ cucumber.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 
-module.exports = {
-    default: `--publish-quiet --parallel ${process.env.instanceCount}`
+if (process.env.instanceCount) {
+    module.exports = {
+        default: `--publish-quiet --parallel ${process.env.instanceCount}`
+    }
+}
+else {
+    module.exports = {
+        default: `--publish-quiet`
+    }
 }
